8aebed2e88757c27cc8a75b73fb318b484b15cdf
yantian001@vip.qq.com <yantian001@vip.qq.com>
2015-11-30 19:08:58 +0800

    2015/11/30

--- Assets/2DShooting/Scripts/Weapon/Weapon.cs
+++ Assets/2DShooting/Scripts/Weapon/Weapon.cs
@@ -5,27 +5,31 @@ public class Weapon : MonoBehaviour {
 
 	//是否可以连续开枪
 	public bool canComboFire = true;
-
+    //枪支的攻击力
 	public float attack = 1.0f;
 	//射击间隔
 	public float shootInterval = 0.2f;
 	//瞄准器的位置
 	public Transform signTransform;
-
-	public Animator anim;
-
+	
+    //枪击的音效
 	public AudioSource fireAudio;
 	//枪口效果位置
 	public GameObject muzzleEffectPlace;
-
+    //枪口的效果
 	public GameObject muzzleEffect;
-	// Use this for initialization
-
+    //子弹打中物体的效果
 	public GameObject impactEffect;
+    //子弹
+    public GameObject bullet;
+
+    //场景对象
+    public GameObject background;
 
 	bool canShoot = false;
 	float deltaTime =0.0f;
-	void Start () {
+    Animator anim;
+    void Start () {
 		if (signTransform == null) {
 			signTransform = GameObject.Find("Sign").transform;
 		}
@@ -68,35 +72,55 @@ public class Weapon : MonoBehaviour {
 		canShoot = false;
 		anim.SetTrigger ("isShooting");
 		PlayMuzzleEffect ();
-//		if (fireAudio != null && !fireAudio.isPlaying) {
-//			fireAudio.Play();
-//		}
 		PlayShootAudio ();
-		//Debug.DrawRay (postion, Vector3.zero,Color.blue);
-		//LayerMask mask = ~(1 << 0);
-		RaycastHit2D rayhit = Physics2D.Raycast (postion, Vector2.zero);
-		RaycastHit2D[] rayhitall = Physics2D.RaycastAll (postion, Vector2.zero);
-		if (rayhitall.Length > 0) {
-			for(int i = 0 ;i < rayhitall.Length;i++)
-			{
-				if(rayhitall[i].collider != null){
-					Debug.Log(rayhitall[i].collider.name);
-				}
-			}
-		}
-		if (rayhit != null && rayhit.collider != null) {
+        ShowBullet();
+        RaycastHit2D rayhit = Physics2D.Raycast (postion, Vector2.zero);
+		if ((rayhit != null) && rayhit.collider != null) {
 			//Debug.Log(rayhit.collider.name);
 			Enemy enemy = rayhit.collider.gameObject.GetComponent<Enemy>();
+           
 			if(enemy != null){
 				enemy.TakeDamage(attack);
 			}
 		}
+        else
+        {
+            PlaySignImpactEffect(postion);
+        }
 
 	}
 
-	void PlaySignImpactEffect(){
-		
-	}
+    //显示子弹，从枪口到准星
+    void ShowBullet()
+    {
+        if(bullet == null)
+        {
+            return;
+        }
+        GameObject bult = Instantiate(bullet) as GameObject;
+        bult.transform.position = muzzleEffectPlace.transform.position;
+        bult.transform.parent = muzzleEffectPlace.transform;
+        
+        //Transform bulletTransform =
+        //bulletTransform.position = signTransform.position;
+        //bulletTransform.localScale = bult.transform.localScale * 0.5f;
+        //子弹运行到准星的位置
+        iTween.MoveTo(bult, iTween.Hash("position", signTransform.position, "time", 0.1, "oncomplete", "OnBulletMoveComplete", "oncompletetarget", gameObject,"oncompleteparams",(System.Object)bult));
+        
+    }
+
+   public void OnBulletMoveComplete(System.Object target)
+    {
+       // Debug.Log("move complete");
+       Destroy((Object)target);
+    }
+
+    //添加子弹击中地面的标记
+	void PlaySignImpactEffect(Vector3 position){
+        GameObject impact = Instantiate(impactEffect) as GameObject;
+        impact.transform.localPosition = background.transform.InverseTransformVector(position);
+        impact.transform.parent = background.transform;
+    }
 
 	//播放射击时枪口特效
 	void PlayMuzzleEffect(){
