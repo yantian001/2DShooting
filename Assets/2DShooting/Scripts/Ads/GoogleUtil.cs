91145b5895b74075e9ba48424662f259b4842cf8
Hugo <yantian001@vip.qq.com>
2015-12-17 22:46:12 +0800

    12/17 night

---
+++ Assets/2DShooting/Scripts/Ads/GoogleUtil.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleMobileAds.Api;
+public class GoogleUtil : MonoBehaviour
+{
+    BannerView banner = null;
+
+    void Start()
+    {
+        AdSize size = new AdSize(300, 400);
+        banner = new BannerView("ca-app-pub-8295605020027148/4747339516", AdSize.Banner, AdPosition.Bottom);
+        AdRequest request = new AdRequest.Builder().Build();
+        banner.LoadAd(request);
+
+    }
+
+    public void OnDisable()
+    {
+        if (banner != null)
+        {
+            banner.Destroy();
+        }
+    }
+}
